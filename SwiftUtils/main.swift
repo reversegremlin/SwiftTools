e1e1743849d80b406dd2da4018d6754742901ad1
reversegremlin <reversegremlin@gmail.com>
2015-02-06 10:20:08 -0800

    fixed quicksort

--- SwiftUtils/main.swift
+++ SwiftUtils/main.swift
@@ -24,11 +24,11 @@ s.push("Sarah")
 s.push("Sally")
 
 
-for index in 1...10 {
-    if let foo = s.pop() {
-        println(foo)
-    }
-}
+//for index in 1...10 {
+//    if let foo = s.pop() {
+//        println(foo)
+//    }
+//}
 
 var rArray: [Int] = []
 
@@ -50,25 +50,6 @@ func shuffle<C: MutableCollectionType where C.Index == Int>(var list: C) -> C {
 
 var nArray = shuffle(rArray)
 
-
-//NSString *dictionary = [NSString stringWithContentsOfFile: @"/usr/share/dict/words"
-//encoding: NSUTF8StringEncoding
-//error: nil];
-//
-//NSArray *words = [NSArray arrayWithArray:[dictionary componentsSeparatedByString:@"\n"]];
-//
-//_randomStrings = [[NSMutableArray arrayWithCapacity:_N] mutableCopy];
-//
-//for (int i = 0; i < _N; ++i) {
-//    int r = arc4random_uniform((int)[words count]);
-//    [_randomStrings addObject:words[r]];
-//}
-
-//var randStrings = ["monkey", "donkey", "spider", "orangutan", "shark", "bee", "fly", "tiger", "lion", "bear"]
-
-
-//Users/hexjunky/Downloads
-
 var text = String(contentsOfFile:"/Users/hexjunky/Downloads/QuickSort.txt", encoding: NSUTF8StringEncoding, error: nil)!
 var stringArray = text.componentsSeparatedByString("\n")
 var intArray = [Int]()
@@ -80,16 +61,22 @@ for intStr in stringArray {
   }
 }
 
+var anotherIntArray = intArray
+
+
 
 
 //quickSortLastElement(&intArray)
 quickSortFirstElement(&intArray)
 
+quickSortLastElement(&anotherIntArray)
+
+
 //quickSort(&intArray)
 //mergeSort(&nArray)
-for thing in intArray {
-    println(thing)
-}
+//for thing in intArray {
+//    println(thing)
+//}
 
 //insertionSort(&randStrings)
 //
